40463c13116b4244facde85f878e18f687e2a5a5
Patrik Majerčík <xmajerc@fi.muni.cz>
2018-11-16 14:29:38 +0100

    Init milestone 2

---
+++ DeliveryService-persistance/src/main/java/cz/muni/fi/pa165/project/dao/impl/DeliveryDaoImpl.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.project.dao.impl;
+
+import cz.muni.fi.pa165.project.entity.Delivery;
+
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.validation.ValidationException;
+import javax.validation.constraints.NotNull;
+
+import cz.muni.fi.pa165.project.dao.DeliveryDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Jakub Gavlas
+ */
+@Repository
+public class DeliveryDaoImpl implements DeliveryDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(@NotNull Delivery delivery) throws IllegalArgumentException, ValidationException {
+        if (delivery == null) {
+            throw new IllegalArgumentException("delivery is null");
+        }
+        if (delivery.getId() != null) {
+            throw new ValidationException("delivery id cannot be set before creation");
+        }
+        em.persist(delivery);
+    }
+
+    @Override
+    public Delivery update(@NotNull Delivery delivery) throws IllegalArgumentException, ValidationException {
+        if (delivery == null) {
+            throw new IllegalArgumentException("delivery is null");
+        }
+        if (delivery.getId() == null) {
+            throw new ValidationException("delivery id is null");
+        }
+        return em.merge(delivery);
+    }
+
+    @Override
+    public void delete(@NotNull Delivery delivery) throws IllegalArgumentException, ValidationException {
+        if (delivery == null) {
+            throw new IllegalArgumentException("delivery is null");
+        }
+        if (delivery.getId() == null) {
+            throw new ValidationException("delivery id is null");
+        }
+        em.remove(delivery);
+    }
+
+    @Override
+    public Delivery findById(@NotNull Long id) throws IllegalArgumentException {
+        if (id == null) {
+            throw new IllegalArgumentException("id is null");
+        }
+        return em.find(Delivery.class, id);
+    }
+
+    @Override
+    public List<Delivery> findAll() {
+        return Collections.unmodifiableList(em.createQuery("SELECT d FROM Delivery d", Delivery.class).getResultList());
+    }
+
+
+}
---
+++ DeliveryService-persistance/src/main/java/cz/muni/fi/pa165/project/entity/Address.java
@@ -0,0 +1,44 @@
+package cz.muni.fi.pa165.project.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Address entity
+ *
+ * @author Patrik Majercik, Tomas Terem
+ */
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Setter(AccessLevel.NONE)
+    @EqualsAndHashCode.Exclude
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String postalCode;
+
+    @NotNull
+    @Column(nullable = false)
+    private String city;
+
+    @NotNull
+    @Column(nullable = false)
+    private String street;
+
+    @NotNull
+    @Column(nullable = false)
+    private String streetNumber;
+    
+}

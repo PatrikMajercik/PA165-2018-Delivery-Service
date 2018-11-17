5ce776c331f704955e91a34a7f39272abcec8a72
Tomas Terem <xterem@fi.muni.cz>
2018-11-17 17:01:40 +0100

    Create AddressService interface

---
+++ DeliveryService-service/src/main/java/AddressService.java
@@ -0,0 +1,47 @@
+import cz.muni.fi.pa165.project.entity.Address;
+
+import java.util.List;
+
+
+/**
+ * Interface for AddressService
+ *
+ * @author Tomas Terem
+ */
+public interface AddressService {
+
+    /**
+     * Create new address in database
+     *
+     * @param address address to create
+     */
+    void create(Address address);
+
+    /**
+     * Updates given address in database
+     *
+     * @param address address to update
+     */
+    void update(Address address);
+
+    /**
+     * Removes given address from database
+     *
+     * @param address address to delete
+     */
+    void delete(Address address);
+
+    /**
+     * Return specific address based on its unique id
+     *
+     * @param id id to search for
+     */
+    Address findById(Long id);
+
+    /**
+     * Return all addresses in database
+     *
+     * @return list of all addresses in database
+     */
+    List<Address> findAll();
+}

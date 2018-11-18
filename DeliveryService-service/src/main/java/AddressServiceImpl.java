86cc04e865fa70f16d0e3db654a5328bcc1c1fcc
Tomas Terem <xterem@fi.muni.cz>
2018-11-18 11:08:17 +0100

    Create AddressServiceImpl

---
+++ DeliveryService-service/src/main/java/AddressServiceImpl.java
@@ -0,0 +1,31 @@
+import cz.muni.fi.pa165.project.dao.AddressDao;
+import cz.muni.fi.pa165.project.entity.Address;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class AddressServiceImpl implements AddressService {
+
+    @Inject
+    private AddressDao addressDao;
+
+    public void create(Address address) {
+        addressDao.create(address);
+    }
+
+    public void update(Address address) {
+        addressDao.update(address);
+    }
+
+    public void delete(Address address) {
+        addressDao.delete(address);
+    }
+
+    public Address findById(Long id) {
+        return addressDao.findById(id);
+    }
+
+    public List<Address> findAll() {
+        return addressDao.findAll();
+    }
+}
